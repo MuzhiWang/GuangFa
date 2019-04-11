f0ce8568b547a54503ecb94e8f44f678dba5eec1
MuzhiWang <MuzhiWang@github.com>
2019-04-11 16:49:18 -0700

    Add JavaFX

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,44 @@ import Tools.Utils;
 import java.io.File;
 import java.util.List;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+    Button button;
+
 
     public static void main(String... args) {
-        List<File> files = Utils.getAllFiles(".");
+        launch(args);
+    }
 
-        if (files.isEmpty()) {
-            System.out.println("No files found! \n");
-        }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("哈哈哈广发发发");
 
-        for (File f : files) {
-            System.out.printf("File found: %s\n", f.getAbsolutePath());
+        this.button = new Button();
+        this.button.setText("Click me");
+        this.button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == this.button) {
+            this.button.setText("Oh yes gogogogo!");
         }
     }
 }
